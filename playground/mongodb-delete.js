b74a467d3637f43cbd7f5417d9cdcf5a6acf0899
s-karki <sah.karki@gmail.com>
2017-01-22 17:06:08 -0500

    updated delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -6,26 +6,29 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) =>{
     }
     console.log("Connected to database server");
 
-    //deleteMany
-    // db.collection("Todos").deleteMany({text: "eat dinner"}).then((res) =>{
-    //     console.log(res);
-    // });
+    //deleteMany (all documents that match the parameter)
+    db.collection("Todos").deleteMany({text: "eat dinner"}).then((res) =>{
+        console.log(res);
+    });
 
-    // //deleteOne (deletes first item it sees that meets the criteria)
-    // // db.collection("Todos").deleteOne({text: "phone mum"}).then((res) => {
-    // //     console.log(res);
-    // // });
+    //deleteOne (deletes first item it sees that meets the criteria)
+    db.collection("Todos").deleteOne({text: "phone mum"}).then((res) => {
+        console.log(res);
+    });
 
-    // //findOneAndDelete --> returns deleted document (value parameter)
-    // db.collection("Todos").findOneAndDelete({completed: "false"}).then((res)=>{
-    //     console.log(res);
-    // });
+    //findOneAndDelete --> returns deleted document (value parameter)
+    db.collection("Todos").findOneAndDelete({completed: "false"}).then((res)=>{
+        console.log(res);
+    });
 
 
-    // db.collection("Users").findOneAndDelete({_id: new ObjectID("5883fc21d4b77159399d286f")})
-    // .then((res) => {
-    //     console.log(res);
-    // });
+
+
+
+    db.collection("Users").findOneAndDelete({_id: new ObjectID("5883fc21d4b77159399d286f")})
+    .then((res) => {
+        console.log(res);
+    });
 
     db.collection("Users").deleteMany({name: "Saharsha"}).then((res) => {
         console.log(res);
