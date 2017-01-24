da4a52474e42071ab53d20bd186de9c9c55c0814
s-karki <sah.karki@gmail.com>
2017-01-24 16:10:48 -0500

    add GET todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -16,32 +16,32 @@ if(!ObjectID.isValid(id)){
 
 
 
-// Todo.find({
-//     _id: id //automatically convert to ObjectID
-// }).then((todos) => {
-//     if(todos.length === 0){
-//         return console.log("ID not found");
-//     }
-//     console.log("Todo find all with id: ", todos);
-// }); //find all results
+Todo.find({
+    _id: id //automatically convert to ObjectID
+}).then((todos) => {
+    if(todos.length === 0){
+        return console.log("ID not found");
+    }
+    console.log("Todo find all with id: ", todos);
+}); //find all results
 
-// Todo.findOne({
-//     _id: id
-// }).then((todo) => {
-//     if(!todo){
-//         return console.log("ID not found");
-//     }
-//     console.log("Todo find one with id: ", todo);
-// });//find one
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    if(!todo){
+        return console.log("ID not found");
+    }
+    console.log("Todo find one with id: ", todo);
+});//find one
 
-// Todo.findById(id).then((todo) =>{
-//     if(!todo){
-//         return console.log("ID not found");
-//     }
-//    console.log("Todo find one with id (slighly better): ", todo); 
-// }).catch((e) => { //check for invalid ID 
-//     console.log(e);
-// }); //find one by ID
+Todo.findById(id).then((todo) =>{
+    if(!todo){
+        return console.log("ID not found");
+    }
+   console.log("Todo find one with id (slighly better): ", todo); 
+}).catch((e) => { //check for invalid ID 
+    console.log(e);
+}); //find one by ID
 
 
 //In users:
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var express = require("express");
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require("mongodb");
 
 var app = express();
 
@@ -34,16 +35,33 @@ app.get("/todos", (req, res)=>{
 
 
 
+app.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){ //validate object
+        res.status(404).send();
+        return console.log("Invalid ID");
+    }
+
+    Todo.findById(id).then((todo) => { //query database
+        if(!todo){
+            res.status(404).send(); //failure case 
+            return console.log("Unable to find todo");
+        }
+        res.status(200).send({todo}); //success case 
+    }, (e) => { //error 
+        res.status(400).send();
+        console.log("Bad request", e);
+    });
+
+});
+
 app.listen(3000, ()=>{
     console.log("Started on port 3000");
 });
 
+
 module.exports.app = app; 
-
-
-
-
-
 
 
 
