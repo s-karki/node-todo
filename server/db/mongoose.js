511306bbfa6b05d441c6c889399b811b67577bdd
s-karki <sah.karki@gmail.com>
2017-01-25 14:35:49 -0500

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise; //native Promise library 
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 //mongoose continuously waits for new connections before making a query.
 //no need to micromanage order. 
 
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {User} = require('./models/user');
 var {ObjectID} = require("mongodb");
 
 var app = express();
+const port = process.env.PORT || 3000; //for heroku 
 
 app.use(bodyParser.json()); //can send json to express app
 
@@ -58,8 +59,8 @@ app.get("/todos/:id", (req, res) => {
 
 });
 
-app.listen(3000, ()=>{
-    console.log("Started on port 3000");
+app.listen(port, ()=>{
+    console.log(`Started on port  ${port}`);
 });
 
 
